ad31977e5c51cb176bb05fb99ff73714f2a2ff52
Matheus <pereiramathe2030@gmail.com>
2026-07-16 20:03:44 -0400

    resolvendo problema do estoque não funcionar

--- includes/footer.php
+++ includes/footer.php
@@ -2,4 +2,4 @@
     <div id="toast-container" class="fixed top-20 right-4 z-[100] flex flex-col gap-3"></div>
 
     <!-- Scripts (ES Modules) -->
-    <script type="module" src="js/app.js"></script>
+    <script type="module" src="js/app.js?v=1.0.1"></script>
